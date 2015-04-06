8295e4174cf7f329135116e5e503a31ff62f3d9c
kortatu <kortatu@gmail.com>
2015-04-06 11:45:05 +0200

    Added params to find methods

--- src/main/java/net/kortsoft/jsonws/BeaconRepository.java
+++ src/main/java/net/kortsoft/jsonws/BeaconRepository.java
@@ -1,8 +1,9 @@
 package net.kortsoft.jsonws;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface BeaconRepository extends JpaRepository<Beacon, Long> {
-	public Beacon findByBeaconId(String beaconId);
-	public Beacon findByUrl(String url);
+	public Beacon findByBeaconId(@Param("beaconId") String beaconId);
+	public Beacon findByUrl(@Param("url") String url);
 }
